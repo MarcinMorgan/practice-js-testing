b77aa9b90772eb998cc3436044e2edaf571fd58f
MarcinMorgan <morgangzit@gmail.com>
2024-06-27 23:22:05 +0200

    t2 done: display error in html document

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,47 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+    try {
+        const clickEl = document.querySelector('.error--click');
+        const enterEl = document.querySelector('.error--enter');
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+        setRandomPosition(clickEl);
+        setRandomPosition(enterEl);
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
+        initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
+        initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
+    } catch(e) {
+        console.log('znalezioniono błąd')
+    }
 
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
+    try {
+        element.style.top = Math.random() * 600 + 'px';
+        element.style.left = Math.random() * 800 + 'px';
 
-    if(error) {
-        throw error;
+        if(error) {
+            throw error;
+        }
+    } catch(e) {
+        displayError(e);
     }
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+    try{
+        element.addEventListener(eventName, function() {
+            setRandomPosition(this, error);
+        })
+    } catch(e) {
+        console.log('błąd')
+    }
+}
+
+function displayError(errorText) {
+    const alertEl = document.querySelector('.alert');
+    
+    alertEl.firstElementChild.innerText = errorText;
+    alertEl.classList.remove('alert--hidden');
 }
