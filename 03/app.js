751f4fb2556630d2ba30b282701c6c7602a759e0
MarcinMorgan <morgangzit@gmail.com>
2024-06-28 01:06:50 +0200

    t3: more tests added

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,11 @@
 export default function randomNumber(min, max) {
-    if(typeof(min) === 'number' && typeof(max) === 'number' && min <= max) {
-        return Math.random() * (max - min) + min;
-    };
+    if(typeof(min) === 'number' && typeof(max) === 'number') {
+        if(min <= max) {
+            return Math.random() * (max - min) + min;
+        } else {
+            throw new RangeError();
+        }
+    } else {
+        throw TypeError();
+    }
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,5 +1,22 @@
 import randomNumber from './app';
 
-test(`check if number`, () => {
+it(`expect number`, () => {
     expect(typeof(randomNumber(1,1))).toBe('number');
+})
+
+it(`random 1,1 to be 1`, () => {
+    expect(randomNumber(1,1)).toBe(1);
+});
+
+it(`expect type error `, () => {
+    expect(() => { randomNumber('NaN',2) }).toThrow();
+});
+
+it(`expect range error`, () => {
+    expect(() => { randomNumber(3,2)} ).toThrow();
+});
+
+it(`check if number in range`, () => {
+    expect(randomNumber(2,4)).toBeGreaterThanOrEqual(2);
+    expect(randomNumber(2,4)).toBeLessThanOrEqual(4);
 })
