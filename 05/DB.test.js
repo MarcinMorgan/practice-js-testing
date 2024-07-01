ce01b74767f89fecc774051d466c51f78a66aea6
MarcinMorgan <morgangzit@gmail.com>
2024-07-01 23:43:19 +0200

    t5: test for insert and getRows, note: test for inserting a row passes/fails randomly, to be resolved

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,1 +1,38 @@
 import DB from './DB';
+
+describe('Insert rows', () => {
+    it('should throw exception when id is incorrect', () => {
+        const item = {id: 'text'};
+        const dataBase = new DB();
+        const promise = dataBase.insert(item);
+        
+        return dataBase.insert(item).catch(err => {
+            expect(err).toBe('ID can be only number!');
+        })
+    })
+})
+
+describe('Select rows', () => {
+})
+
+describe('Remove rows', () => {
+})
+
+describe('Update rows', () => {
+})
+
+describe('Truncate rows', () => {
+})
+
+describe('Get rows', () => {
+    it('should include item', () => {
+        const item = {id: 2};
+        const dataBase = new DB(item);
+        const promise = dataBase.getRows();
+
+        promise.then(result => {
+            expect(result).toContain(item);
+            done();
+        })
+    })
+})
