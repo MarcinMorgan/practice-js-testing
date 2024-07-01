b2db082c6f211305f39bb5dca32a129186107e55
MarcinMorgan <morgangzit@gmail.com>
2024-07-01 22:24:34 +0200

    t4 done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,38 @@
 export default class User {
+    constructor({email, password}) {
+        this.email = checkEmail(email);
+        this.password = checkPassword(password);
+    }
 
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    login() {
+        if(this.email.includes('@devmentor.pl')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+function checkEmail(email) {
+    if (email.includes('@')) {
+        return email;
+    } else {
+        throw new Error('Niepoprawny email');
+    }
+}
+
+function checkPassword(password) {
+    if (password.length > 6) {
+        return password;
+    } else {
+        throw new Error('Za krótkie hasło');
+    }
 }
